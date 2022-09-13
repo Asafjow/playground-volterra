c7044e349c39907219e9e53fc86c341a0e145bd7
Asafjow <35654980+Asafjow@users.noreply.github.com>
2022-09-13 17:07:57 +0300

    Update variables.tf

--- HTTP_and_Origin_Pool/LB_EXTERNAL_HTTPS_autocert_new_WAAP_and_clusterRoute/variables.tf
+++ HTTP_and_Origin_Pool/LB_EXTERNAL_HTTPS_autocert_new_WAAP_and_clusterRoute/variables.tf
@@ -1,14 +1,14 @@
 variable "api_cert" {
             type = string
-            default = "/Users/sahar/Documents/asaf-vol-apicert.crt"
+            default = "/vol-apicert.crt"
         }
         
         variable "api_key" {
           type = string
-          default = "/Users/sahar/Documents/asaf-vol-apikey.key"
+          default = "/vol-apikey.key"
         }
         
         variable "api_url" {
             type = string
-            default = "https://f5-emea-ent.console.ves.volterra.io/api"
+            default = "https://mytenant.console.ves.volterra.io/api"
         }
